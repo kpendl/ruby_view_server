0ffced274f927eb95a42f63cad1489b0f65828fc
MakerSquare Student <student@lab-7.att.net>
2013-06-25 11:55:26 -0500

    playing with simple and advanced servers

--- server_simple.rb
+++ server_simple.rb
@@ -1,7 +1,7 @@
 require 'webrick'
 
 
-## Set the path to serve files from to the public directory in this project
+##  the path to serve files from to the public directory in this project
 root = File.expand_path 'public'
 
 # Create a new server on port 8000
@@ -10,5 +10,8 @@ server = WEBrick::HTTPServer.new :Port => 8000, :DocumentRoot => root
 ## Capture control+c to shut down the server
 trap 'INT' do server.shutdown end
 
+server.mount_proc '/' do |req, res|
+  res.body = 'Hello, world!'
+end
 ## Start the server
 server.start
